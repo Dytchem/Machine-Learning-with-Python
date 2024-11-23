f3f8f60548f62126ae504a1ea9c89671c6ea4750
Dytchem <1054329487@qq.com>
2024-11-23 16:11:20 +0800

    mooc

--- crawler/mooc/.ipynb_checkpoints/getAnswer-checkpoint.py
+++ crawler/mooc/.ipynb_checkpoints/getAnswer-checkpoint.py
@@ -7,7 +7,7 @@ def getAnswer(aId):
     url = "http://www.icourse163.org/mob/course/paperDetail/v1"
     headers = {"Content-Type": "application/x-www-form-urlencoded; charset=UTF-8"}
     data = (
-        "mob-token=0f09b2b7630c129548d74dddd9eb97619a7891423fd6b617a0b8bde4b64c45ae398b3d63cb5fd52eef52676df13af54c76a799785561dc0ef8c10c98d0192f7529052a64e3eb0e2f6fdddaee44a6ed1e3fe82d8881bd18d30e2ebd9980e23f4b0d7ef1ee30366d043e17db84286433ce431fa5380bde4a7f17bdd49a3580fc84bdf8e751bb17227e812f9f3526a4c2d0a44713ec5840b51a00087d8e176e19a38676b27e0d374f2f0db184284eeb568ef068144cfcbaf5b449620a83b20dcbf5&testId=1242838683&isExercise=false&aId="
+        "mob-token=8817e1a1d01e93af76dd102b05a6fb886d0b25fd8b19b252175ba33f4e18dedf2543d4ea7e783a58c8551b3f45bb3de406322198119ae64f6263f8326e676386f3276f874c0ec7bb0d95be9d6e4d91cfd75c76b58fbbd619c321b614429bde74ff0d400dfd279a363cbfeb42eb5bf2a7332c51a6107e975449698118cf648d5f0088e9975dce110878690b966a534b57e9752b3e5df78238b6c877f6c588079c24aa40074fc2d585cb89833cb42e25ef571d6d864a360b95ea2b933488934850abdae0793dfb09f8643ced6884c79b818275a1d5be7805387fd996151d7307b0&testId=1242838683&isExercise=false&aId="
         + str(aId)
         + "&withStdAnswerAndAnalyse=true&"
     )
@@ -26,8 +26,11 @@ def getAnswer(aId):
         print(t, ":\t", end=" ")
         for j in L:
             print(j, end=" ")
-        print()
-    pprint(q)
+        if i["stdAnswer"]:
+            print(i["stdAnswer"])
+        else:
+            print()
+    # pprint(q)
 
 
 if __name__ == "__main__":
